40bf574688941b618f8c3c7a410c71f8d316a14c
Casstiel Pi <cpss5433@gmail.com>
2026-07-28 10:10:54 -0700

    add transaction history, deposit, withdraw, view act blnce endpoints

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
+
+
+
+# helper functions
+def get_user(user_id: int):
+
+    for user in users:
+        if user["id"] == user_id:
+            return user
+
+    raise HTTPException(
+        status_code = 404,
+        detail = "User Not Found"
+    )
+
 
 # DTO
 class UserCreate(BaseModel):
     name: str
     email: str
 
+
+class TranscationCreate(BaseModel):
+    amount: float = Field(gt = 0)
+
+# temp seeder data
 users = [
     {
         "id": 1,
         "name": "John",
-        "email": "john@sample.com"
+        "email": "john@sample.com",
+        "balance": 1000
     },
     {
         "id": 2,
         "name": "Jane",
-        "email": "jane@sample.com"
+        "email": "jane@sample.com",
+        "balance": 1000
     }
+]
+
+transactions = [
+
 ]
 
 # default route
@@ -95,3 +121,78 @@ def delete_user(user_id: int):
         status = 404,
         detail = "User Not Found"
     )
+
+
+# deposit funds
+@app.post('/users/{user_id}/deposit')
+def deposit_funds(user_id: int, transaction: TranscationCreate):
+    user = get_user(user_id)
+
+    user["balance"] += transaction.amount
+
+    new_transaction = {
+        "id": len(transactions) + 1,
+        "user_id": user_id,
+        "type": "deposit",
+        "amount": transaction.amount,
+        "balance_after": user["balance"]
+    }
+
+    transactions.append(new_transaction)
+
+    return {
+        "message": "Deposit successful",
+        "transaction": new_transaction
+    }
+
+
+# withdraw funds
+@app.post("/users/{user_id}/withdraw")
+def withdraw_funds(user_id: int, transaction: TranscationCreate):
+    user = get_user(user_id)
+
+    if transaction.amount > user["balance"]:
+        raise HTTPException(
+            status = 400,
+            detail = "Insufficient funds"
+        )
+
+    user["balance"] -= transaction.amount
+
+    new_transaction = {
+        "id": len(transactions) +1,
+        "user_id": user_id,
+        "type": "withdrawal",
+        "amount": transaction.amount,
+        "balance_after": user["balance"]
+    }
+
+    transactions.append(transaction)
+
+    return {
+        "message": "Withdrawal successful",
+        "transaction": new_transaction
+    }
+
+
+# get user transaction history 
+@app.get('/users/{user_id}/transactions')
+def get_user_transaction_history(user_id: int):
+    get_user(user_id)
+
+    user_transactions = [
+        transaction for transaction in transactions
+        if transaction["user_id"] == user_id
+    ]
+
+    return user_transactions
+
+# get user account balance
+@app.get("/users/{user_id}/balance")
+def get_user_account_balance(user_id: int):
+
+    user = get_user(user_id)
+
+    return {
+        "balance": user["balance"]
+    }
